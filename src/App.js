f6f51eeb175ecc47dd52f8ecdff1724a7b1aa6ed
mahesh16 <mahesh@quarkstudios.com>
2021-01-27 11:14:31 +0530

    Implement Routing In AAPP

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Movies from "./components/movies";
+import TopNavbar from "./components/topnavbar";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import MoviesForm from "./components/moviesForm";
 
 function App() {
   return (
     <div>
-      <Movies></Movies>
+      <TopNavbar />
+      <Switch>
+        <Route path="/customers" component={Customers} />
+        <Route path="/rentals" component={Rentals} />
+        <Route path="/movies/:id" component={MoviesForm} />
+        <Route path="/movies" component={Movies} />
+        <Route path="/page-not-found" component={NotFound} />
+        <Route path="/" exact component={Movies} />
+        <Redirect to="/page-not-found" />
+      </Switch>
     </div>
   );
 }
